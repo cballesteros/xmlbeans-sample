2c8408a97d152a22c99e9b291e19662b27995714
Cristian Ballesteros <cballest@pros.com>
2017-10-19 17:24:33 -0500

    first changes

---
+++ xmlbeansPlugin/cmdGradle421.bat
@@ -0,0 +1,5 @@
+echo off
+@title = Command for gradle 4.2.1
+set GRADLE_HOME=C:\gradle-4.2.1
+set PATH=%PATH%;%GRADLE_HOME%\bin
+cmd
---
+++ xmlbeansPlugin/sample/XmlBeansGradle/src/main/java/com/example/MainClass.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.example.xmlBeans.UserDocument;
+
+public class MainClass
+{
+    public MainClass()
+    {
+        UserDocument userDocument;
+    }
+}
